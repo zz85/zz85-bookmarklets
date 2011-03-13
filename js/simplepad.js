252e608e0fc93cd31bfa4f4b0d5b7c9655836fe8
zz85 <zz85nus@gmail.com>
2011-03-13 01:11:45 -0800

    added simplepad.js

---
+++ js/simplepad.js
@@ -0,0 +1,15 @@
+/*
+ * This is a really simple bookmarklet which imitates the use of notepad
+ * on windows for simple plain text editing. 
+ * The reason I wrote this is because of my recent usage using a mac 
+ * where textedit serves html formatting by default.
+ * This "simplepad" is then great for stripping formatting from text
+ * and editing with plain text.
+ * If you have a suggession for a better name - easypad, simplepad
+ * or my original name, bigpad, do let me know.
+ *
+ * you can run this with javascript:document.write('<textarea style="width:100%;height:100%"></textarea>');
+ * or javascript:document.write('<script src="https://github.com/zz85/zz85-bookmarklets/js/simplepad.js"></script>');
+ *
+ * 12 March 2011
+document.write('<textarea style="width:100%;height:100%"></textarea>');
